18c82c13ff93efb6fef8be71125be086ed9ccd25
hitoshi <g.hitoshi.yokogawa@gmail.com>
2020-10-20 18:45:17 -0300

    Páginas prontas

---
+++ nyous-react/src/components/rodape/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rodape = () => {
+    return(
+        <footer className="text-center" style={{marginTop : '70px'}}>
+            <h1>SENAI Informática</h1>
+            <small>Desenvolvido por <strong>Gabriel Hitoshi</strong></small>
+        </footer>
+    );
+}
+
+export default Rodape;
